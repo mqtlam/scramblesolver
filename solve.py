a948cd72040561f1c05e3c70b6d99dfed215b426
mqtlam <michaelqtlam@gmail.com>
2012-06-15 23:06:53 -0700

    Fixed argument checking.

--- solve.py
+++ solve.py
@@ -2,13 +2,13 @@
 import scramblesolver
 import sys
 
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
 	print 'Usage: ./scramblesolver abcdefghijklmnoqu'
 	sys.exit(1)
 
 board = sys.argv[1]
 special = '0000000000000000'
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
 	special = sys.argv[2]
 
 s = scramblesolver.ScrambleSolver()
